0d1666ed2c01d982d19247ec42b9acffe17d5a53
cminhtruong <cminh.truong@gmail.com>
2017-11-21 18:46:03 +0100

    Finish the design part Create a .gitignore file and README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a simple example in Android Development.
+In this case, I'm not using the OOP but I'm still using Butterknife for an example of third part library in Android.
+
+Enjoy you guys. Thanks
--- app/src/main/java/calc/com/davidetruong/tcalculator/MainActivity.java
+++ app/src/main/java/calc/com/davidetruong/tcalculator/MainActivity.java
@@ -3,6 +3,14 @@ package calc.com.davidetruong.tcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author davidetruong
+ * @version 1.0
+ * @see MainActivity
+ * @since 21 November 2017
+ * My main activity using ButterKnife to show a simple example in Android development app.
+ * Nothing special, enjoy.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
